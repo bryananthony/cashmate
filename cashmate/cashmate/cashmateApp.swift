08af9c70a58cf16bbd729244b497c4ce0383554b
adhiputra19 <apratama05@student.ciputra.ac.id>
2025-05-30 14:03:24 +0700

    Update cashmateApp.swift

--- cashmate/cashmate/cashmateApp.swift
+++ cashmate/cashmate/cashmateApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct cashmateApp: App {
+    var sharedModelContainer: ModelContainer = {
+        do {
+            return try ModelContainer(
+                for: Expense.self, BudgetSetting.self, Goal.self, Saving.self,
+                configurations: ModelConfiguration(isStoredInMemoryOnly: false)
+            )
+        } catch {
+            print("Failed to create ModelContainer: \(error.localizedDescription)")
+            return try! ModelContainer(for: Expense.self, configurations: .init(isStoredInMemoryOnly: true))
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(sharedModelContainer)
         }
     }
 }
