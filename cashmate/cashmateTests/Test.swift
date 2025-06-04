701ba5a458350d5bf9bd4a88bcfc19d310007b36
bryananthony <bryananthony872@gmail.com>
2025-06-04 13:02:03 +0700

    Fix expenses, history

---
+++ cashmate/cashmateTests/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  cashmateTests
+//
+//  Created by student on 04/06/25.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
